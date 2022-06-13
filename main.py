a4446fdd8aabea7da3a0ef606302539d00f7d453
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-06-13 23:32:21 +0430

    Added stuff to the repository

---
+++ main.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import os
+import pyaudio 
+import requests
+import ssl
+import urllib.parse
+import wave
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
